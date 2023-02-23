b10479e00a598276b56efbda2b98d8999f7e8e92
Phuc Dong <dongvuminhphuc@gmail.com>
2023-02-23 16:39:29 +0700

    Test new features

--- scripts.js
+++ scripts.js
@@ -9,6 +9,8 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 let playerCount = 0;
 let computerCount = 0;
+let buttons = document.querySelectorAll("button");
+let body = document.querySelector("body");
 
 function playerCounter() {
     playerCount++;
@@ -22,60 +24,72 @@ function computerCounter() {
     return computerCount;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    console.log("Player: " + playerSelection + ". Computer: " + computerSelection);
+function displayPlayerResult() {
+    let div3 = document.createElement("div");
+    playerCounter();
+    div3.textContent = `Player wins!`;
+    body.appendChild(div3);
+}
 
-    if (playerSelection === computerSelection) {
-        console.log("It's a draw!");
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        playerCounter();
-        console.log("Player wins!");
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        playerCounter();
-        console.log("Player wins!");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        playerCounter();
-        console.log("Player wins!");
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        computerCounter();
-        console.log("Computer wins!");
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        computerCounter();
-        console.log("Computer wins!");
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        computerCounter();
-        console.log("Computer wins!");
-    } else {
-        console.log("Invalid input! Choose again.");
+function displayComputerResult() {
+    let div3 = document.createElement("div");
+    computerCounter();
+    div3.textContent = `Computer wins!`;
+    body.appendChild(div3);
+}
+
+function displayFinalResult(playerCount, computerCount) {
+    let div4 = document.createElement("div");
+
+    if (playerCount === 5) {
+        console.log(playerCounter());
+        div4.textContent = `Player reachs 5 points first. Player wins!`;
+        body.appendChild(div4);
+    } else if (computerCount === 5) {
+        console.log(computerCounter());
+        div4.textContent = `Computer reachs 5 points first. Computer wins!`;
+        body.appendChild(div4);
     }
 }
 
-// console.log(playRound("paper", getComputerChoice()));
+function playRound(playerSelection, computerSelection) {
+    let div1 = document.createElement("div");
+    let div2 = document.createElement("div");
+    div1.textContent = `Player: ${playerSelection}. Computer: ${computerSelection}`;
+    body.appendChild(div1);
 
+    if (playerSelection === computerSelection) {
+        div2.textContent = `It's a draw!`;
+        body.appendChild(div2);
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        displayPlayerResult();
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        displayPlayerResult();
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        displayPlayerResult();
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        displayComputerResult();
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        displayComputerResult();
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        displayComputerResult();
+    }
+
+    displayFinalResult(playerCount, computerCount);
+}
+
+// Playing games
 function game() {
     let playerInput, computerInput;
+    // let div3 = document.createElement("div");
 
-    for (let i = 0; i < 5; i++) {
-        // ask user for input
-        playerInput = prompt("Enter your choice:");
-        computerInput = getComputerChoice();
-
-        // call playRound() function
-        playRound(playerInput, computerInput);
-    }
-
-    // Print the winner after 5 rounds
-    let finalResult;
-    if (playerCounter() > computerCounter()) {
-        finalResult = "Player wins!";
-    } else if (playerCounter() === computerCounter()) {
-        finalResult = "It's a draw!";
-    } else {
-        finalResult = "Computer wins!";
-    }
-
-    console.log("After 5 rounds: " + finalResult);
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            computerInput = getComputerChoice();
+            playerInput = button.textContent;
+            playRound(playerInput, computerInput);
+        });
+    });
 }
 
 game();
